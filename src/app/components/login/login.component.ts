97885476e5a964e77de2b6258b1040c0e9e2cd80
Gregory Lacerda <gregorylacerda@hotmail.com>
2022-11-23 20:44:21 -0300

    autenticação de login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,3 +1,5 @@
+import { Router } from '@angular/router';
+import { AuthService } from './../../services/auth.service';
 import { Credenciais } from './../../models/credenciais';
 import { Component, OnInit } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
@@ -18,21 +20,24 @@ export class LoginComponent implements OnInit {
   email = new FormControl(null, Validators.email);
   senha = new FormControl(null, Validators.minLength(3));
 
-  constructor(private toast: ToastrService) { }
+  constructor(
+    private toast: ToastrService,
+    private service: AuthService,
+    private router: Router) { }
 
   ngOnInit(): void {
   }
   
   logar(){
-    this.toast.error('Usuario e/ou senha inválidos!', 'login');
-    this.creds.senha = '';
+    this.service.authenticate(this.creds).subscribe(resposta => {
+      this.service.successfulLogin(resposta.headers.get('Authorization').substring(7))
+      this.router.navigate([''])
+    }, () => {
+      this.toast.error('Usuário e/ou senha inválidos');
+    })
   }
 
   validaCampos(): boolean{
-    if (this.email.valid && this.senha.valid) {
-      return true;
-    }else{
-      return false;
-    }
+    return this.email.valid && this.senha.valid
   }
 }
